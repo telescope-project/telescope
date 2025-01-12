fbba7a0d571c4fb893a4bf3aab71e5320738f085
-akku- <151517253+akku1139@users.noreply.github.com>
2025-01-12 15:29:06 +0000

    wip: schema

--- src/lib/server/db/schema/index.ts
+++ src/lib/server/db/schema/index.ts
@@ -1,1 +1,18 @@
-export {}
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core"
+import { relations } from "drizzle-orm"
+
+export const accounts = sqliteTable("accounts", {
+  id: integer().primaryKey(),
+})
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  profiles: many(accounts),
+}))
+
+export const profiles = sqliteTable("profiles", {
+  id: integer().primaryKey(),
+  account: integer().references(() => accounts.id),
+  displayName: text("display_name"),
+  usetname: text(),
+  server: text(),
+})
